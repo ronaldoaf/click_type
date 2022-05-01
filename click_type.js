fee274d9b7914c4193f6c44987072d95b7d321a4
ronaldoaf <ronaldo.a.f@gmail.com>
2022-05-01 16:21:15 -0300

    v2

--- click_type.js
+++ click_type.js
@@ -1,36 +1,138 @@
-var robot = require("robotjs");
-
-var express = require('express');
+const { keyboard, mouse, Point } = require("@nut-tree/nut-js");
+ 
+const express = require('express');
 
 const app=express();
+const WindMouse = require("windmouse");
+const jStat =require('jstat');
 
-robot.setMouseDelay(2);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-app.get('/click', (req, res)=>{
-	const mouse=robot.getMousePos();
+
+mouse.config.mouseSpeed=200
+keyboard.config.autoDelayMs=100;
+
+
+const randomNormal=(x_min, x_max)=>{
+   //const [x_min,x_max]=[0,200];
+   const media=(x_min+x_max)/2;
+   const desv=(media-x_min)/2;
+
+   const a=jStat.normal.cdf( x_min, media, desv );
+   const b=jStat.normal.cdf( x_max, media, desv );
+
+   const p=a+Math.random()*(b-a);
+   return Math.round(jStat.normal.inv( p, media, desv ));
+}
+
+/*
+const mouseMove=async(X, Y)=>{
+   // Initialize WindMouse class
+   const windMouse = new WindMouse(Math.floor(Math.random()*10) );
+   
+   const {x,y}=await mouse.getPosition();
+   
+   // MouseSettings
+   const  settings = {
+     startX: x, startY: y,
+     endX: X,   endY: Y,
+     
+     gravity: 10+Math.ceil(Math.random()*5),
+     wind: 20+Math.ceil(Math.random()*10),
+     //minWait: 1,
+    // maxWait: 1,
+     maxStep: 2+Math.ceil(Math.random()*2),
+     targetArea: 10+Math.ceil(Math.random()*20),
+   } 
+   
+   const points=(await windMouse.GeneratePoints(settings)).map(p=>new Point(p[0],p[1]) );
+   points.push( new Point(X+1,Y+1) );
+   await mouse.move( points);
+}
+*/
+
+const mouseMove=async(X, Y)=>{
+   await mouse.move( [new Point(X,Y)] );
+}
+
+
+app.get('/click_area', async(req, res)=>{
+	const {x1,x2,y1,y2}=req.query;
+   const X=randomNormal(Number(x1), Number(x2));
+   const Y=randomNormal(Number(y1), Number(y2));
+   
+   await mouseMove(X, Y);
+   await sleep(1000);
+   
+   await mouse.leftClick();
+   
+   console.log([x1,x2,y1,y2]);
+	res.send(`console.log("x:${X} y:${Y}")`);
+});	
+
+
+app.get('/scroll', async(req, res)=>{
+	const {y}=req.query;
+
+   await mouse.scrollDown(Number(y));
+   
+	res.send(`console.log("y:${y}")`);
+});	
+
+
+
+app.get('/click', async(req, res)=>{
+	const ponto_incial=await mouse.getPosition();
 	const {x,y}=req.query;
-	robot.moveMouse(Number(x), Number(y) );
-	robot.mouseClick();
-	robot.moveMouse(mouse.x, mouse.y );
-	
+	const ponto_final=new Point(Number(x),Number(y));
+   await mouse.setPosition(ponto_final);
+   
+   await mouse.leftClick();
+   await mouse.setPosition(ponto_incial);
+   
 	res.send(`console.log("x:${x} y:${y}")`);
 });	
 
 
-app.get('/move', (req, res)=>{
+app.get('/move', async(req, res)=>{
 	const {x,y}=req.query;
-	robot.moveMouse(Number(x), Number(y) );
-	//robot.mouseClick();
+   
+   await mouseMove(Number(x),Number(y));
+   /*
+	const ponto_final=new Point(Number(x),Number(y));
+   await mouse.setPosition(ponto_final);
+   */
 	res.send(`console.log("x:${x} y:${y}")`);
 });	
 
 
 
-app.get('/type', (req, res)=>{
-	const {string}=req.query;
-	robot.typeString(string);
-	res.send(`console.log("string:${string}")`);
+app.get('/type', async(req, res)=>{
+	const {str}=req.query;
+   await keyboard.type(str);
+   console.log(str);
+	res.send(`console.log("str:${str}")`);
 });	
+
+
+let token_state='free';
+
+app.get('/token/state', async(req, res)=>{
+   res.send(`localStorage.token_state='${token_state}';`);
+});	
+
+app.get('/token/hold', async(req, res)=>{
+   token_state='hold';
+   setTimeout(()=> token_state='free', 30*1000);
+   res.send(`localStorage.token_state='${token_state}';`);
+});	
+app.get('/token/free', async(req, res)=>{
+   token_state='free';
+   res.send(`localStorage.token_state='${token_state}';`);
+});	
+
 
 app.listen(1313);
 
@@ -47,4 +149,8 @@ console.log("                              |___/|_|         ");
 console.log("\n\n\n");
 console.log("Use:  http://localhost:1313/click?x=100&y=150");
 console.log("Use:  http://localhost:1313/move?x=100&y=150");
-console.log("Use:  http://localhost:1313/type?sting=texto");
+console.log("Use:  http://localhost:1313/type?str=texto");
+console.log("Use:  http://localhost:1313/click_area?x1=10&y1=10&x2=50&y2=50");
+console.log("Use:  http://localhost:1313/token/state");
+console.log("Use:  http://localhost:1313/token/hold");
+console.log("Use:  http://localhost:1313/token/free");
